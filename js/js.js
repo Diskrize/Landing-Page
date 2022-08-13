1e63c1bc8ddbd97722bec2249108d8cd882f99ab
Diskrize <ignatenko.d.1990@gmail.com>
2022-08-13 10:59:09 +0300

    alt

--- js/js.js
+++ js/js.js
@@ -2,11 +2,11 @@ let time = new Date()
 
 let theme = document.getElementById('theme');
 
-if (time.getHours() >= 21 || time.getHours() <= 6 ) {
+if (time.getHours() >= 21 || time.getHours() <= 6) {
     theme.href = "css/dark-them.css"
-    document.getElementById("header_img_1").src="img/bitcoin_dark.png"
-    document.getElementById("header_img_2").src="img/cryptocom_dark.png"
-    document.getElementById("header_img_3").src="img/coinbase_dark.png"
+    document.getElementById("header_img_1").src = "img/bitcoin_dark.png"
+    document.getElementById("header_img_2").src = "img/cryptocom_dark.png"
+    document.getElementById("header_img_3").src = "img/coinbase_dark.png"
 } else {
     theme.href = "css/style.css"
 }
